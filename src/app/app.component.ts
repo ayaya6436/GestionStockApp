ef3074ae58d94ab9570021c48c2d9fca2bc5154c
ayaya <ayaya6436@gmail.com>
2023-08-29 20:07:44 +0000

    dashboard

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,19 +8,18 @@ import { Router, NavigationEnd } from '@angular/router';
 })
 export class AppComponent {
   sideBarStatus: boolean = false; // Par défaut, la barre latérale est fermée
-  showHeader: boolean = true; // Par défaut, afficher le header
 
   constructor(private router: Router) {
     router.events.subscribe((event) => {
       if (event instanceof NavigationEnd) {
         if (event.url.includes('/login') || event.url.includes('/register') || event.url.includes('/forgot-password') || event.url.includes('/verify-email')) {
           this.sideBarStatus = false;
-          this.showHeader = false; // Masquer le header et la barre latérale sur les pages de connexion, d'inscription, de réinitialisation du mot de passe et de vérification d'email
-        } else {
+        } else if (!this.sideBarStatus) {
           this.sideBarStatus = true;
-          this.showHeader = true; // Afficher le header sur les autres pages
         }
       }
     });
   }
+
+
 }
